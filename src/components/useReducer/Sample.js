d636700860bb12378f0ce43ce3aac02fe893d9ee
Abdul-rehman67 <ar6373150@gmail.com>
2021-11-18 13:04:08 +0500

    initial

---
+++ src/components/useReducer/Sample.js
@@ -0,0 +1,15 @@
+import React, { useReducer } from 'react'
+
+const Sample = () => {
+    const reducer =()=>[
+        
+    ]
+const [state , dsipatch]=useReducer(reducer,{count:0,showText:""})
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Sample
